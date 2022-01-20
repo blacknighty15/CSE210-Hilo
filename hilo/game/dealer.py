715372ef2731d10d0d964313c8dd903e181ebad0
Emanuel Herrera <71576817+blacknighty15@users.noreply.github.com>
2022-01-20 12:36:26 -0600

    Add files via upload

---
+++ hilo/game/dealer.py
@@ -0,0 +1,37 @@
+from game.hilo import Hilo
+
+
+class Dealer:
+    """A
+
+
+    """
+
+    def __init__(self):
+        """Constructs a new Dealer.        
+        
+        """
+        
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        """
+        
+
+    def get_inputs(self):
+        """Ask the user if they want to play again
+
+        """
+        
+       
+    def do_updates(self):
+        """Updates the player's score.        
+        """
+        
+
+    def do_outputs(self):
+        """Displays the results.
+
+        """
+        
---
+++ hilo/game/hilo.py
@@ -0,0 +1,20 @@
+import random
+
+
+class Hilo:
+    """
+    Is a game in which the player guesses if the next card drawn by the dealer will be higher or lower than the previous one.
+
+    """
+
+    def __init__(self):
+        """Constructs a new instance of Hilo.
+
+        """
+        
+
+    def draw(self):
+        """
+        Draw again
+
+        """
